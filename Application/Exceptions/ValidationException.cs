109fc203c58ecf7949761b638193c6455f069a66
JULIO C <desarrolladoresap2@outlook.es>
2022-01-14 17:55:26 -0600

    Se crea canalizador de excepciones

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() : base("Se ha producido uno o más errores de validación")
+        {
+            Errors = new List<string>();
+        }
+        public List<string> Errors { get; }
+
+        public ValidationException(IEnumerable<ValidationFailure> farirures) : this()
+        {
+            foreach (var farirure in farirures)
+            {
+                Errors.Add(farirure.ErrorMessage);
+            }
+        }
+    }
+}
