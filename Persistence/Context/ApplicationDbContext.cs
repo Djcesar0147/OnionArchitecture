da8174fb574be6588b08497497dbf5b72e3f9a29
JULIO C <desarrolladoresap2@outlook.es>
2022-01-15 16:17:18 -0600

    Install EntityFrameworkCore

---
+++ Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //Probar el control de rastreador de cambios durante saveChanges()
+            //ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+    }
+}
