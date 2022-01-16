c60715039d2297a24ac4b14f926efed5a0ce6525
JULIO C <desarrolladoresap2@outlook.es>
2022-01-15 18:20:57 -0600

    First Migration

--- Domain/Common/AuditableBaseEntity.cs
+++ Domain/Common/AuditableBaseEntity.cs
@@ -2,9 +2,14 @@
 {
     public class AuditableBaseEntity
     {
+        [MaxLength(200)]
         public string CreadoPor { get; set; }
+
         public DateTime FechaCaptura { get; set; }
+
+        [MaxLength(200)]
         public string? ModificadoPor { get; set; }
+
         public DateTime? FechaModificacion { get; set; }
     }
 }
---
+++ Persistence/Migrations/20220116001915_initialDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persona",
+                columns: table => new
+                {
+                    persona_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    persona_APaterno = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    persona_AMaterno = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    persona_Nombres = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreadoPor = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    FechaCaptura = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificadoPor = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    FechaModificacion = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persona", x => x.persona_Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persona");
+        }
+    }
+}
